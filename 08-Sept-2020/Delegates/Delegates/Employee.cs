18dac17f96995a52ce3448ee1f34fc3580b5857c
Utkarsh_Pant <utkarshpant1994@gmail.com>
2020-09-08 19:09:16 +0530

    Concurrent Collection programs, Events and Delegates

---
+++ 08-Sept-2020/Delegates/Delegates/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    public static void main(string[] args)
+    {
+        List<Employee> l = new List<Employee>();
+
+    }
+
+}
+    
+    class Employee
+    {
+
+        public int ID { get; set; }
+        public String Name { get; set; }
+
+        public String Salary { get; set; }
+
+        public String Experience { get; set; }
+
+        public static void PromoteEmployees(List<Employee> empList) {
+
+            foreach (Employee emp in empList) { 
+            
+                if(emp.Experience >= 2)
+                {
+                    Console.WriteLine(emp.Name +" is Promoted");
+                }
+            }
+        }
+    }
+
---
+++ 08-Sept-2020/Delegates/Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+
+    //......A delegate is a function pointer and it is type safe
+
+    public delegate void HelloFunctionDelegate(string message);
+
+    //....This Delegate can call any function with 'void' return type and string argument
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HelloFunctionDelegate dl = new HelloFunctionDelegate(Hello);
+
+            dl("Hello method called by Delegate ");
+
+            dl("Hello from Delegate !!..... "); /*
+                                                 Delegate will internally call Hello method
+
+                                                 */
+
+            Console.ReadKey();
+        }
+
+        static void Hello(string message) {
+
+            Console.WriteLine(message);
+        }
+    }
+}
