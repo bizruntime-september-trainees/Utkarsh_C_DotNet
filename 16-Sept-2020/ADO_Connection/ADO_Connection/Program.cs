c8d8ed620d2483b9d73d2bbfa95d76893c194176
Utkarsh_Pant <utkarshpant1994@gmail.com>
2020-09-17 16:33:43 +0530

    ADO.net programs

---
+++ 16-Sept-2020/ADO_Connection/ADO_Connection/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient; //.. Includes DataProvider classes Connection, Command, DataAdapter, DataReader, SQLException
+
+
+namespace ADO_Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+
+            Console.ReadKey();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+
+                //....data source = server_name, database= Database_name, integrate security = If using MSSql server, no need to mention username and password
+                con = new SqlConnection("data source=.; database= Student; integrated security=true");
+
+                // writing sql query  and passing Connection object
+
+                SqlCommand cm = new SqlCommand("create table student(id int not null,\n" +
+                    "name varchar(100), \n" +
+                    "email_id varchar(90),\n" +
+                    "join_date Date)",con);
+
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+
+                int rows_affected = cm.ExecuteNonQuery();
+                Console.WriteLine("Rows affected: "+rows_affected);
+                // Displaying a message  
+                Console.WriteLine("Table created Successfully");
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." +e.Message);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ 16-Sept-2020/ADO_Retrieve/ADO_Retrieve/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;  
+using System.Data.SqlClient;  
+
+
+namespace ADO_Retrieve
+
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                new Program().CreateTable();
+            }
+            public void CreateTable()
+            {
+                SqlConnection con = null;
+                try
+                {
+                    // Creating Connection  
+                    con = new SqlConnection("data source=.; database= Student; integrated security=true");
+                    // writing sql query  
+                    SqlCommand cm = new SqlCommand("Select * from student", con);
+                    // Opening Connection  
+                    con.Open();
+
+                    // Executing the SQL query  (Sending the command text to the Connection and builts SqlDataReader object
+
+                    SqlDataReader sdr = cm.ExecuteReader();
+                    // Iterating Data  
+                    while (sdr.Read())
+                    {
+                    //.......DataReader_object[column_name]
+
+                        Console.WriteLine(sdr["id"] + " " + sdr["name"] + " " + sdr["email_id"] +" "+sdr["join_date"]); // Displaying Record  
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("OOPs, something went wrong.\n" + e);
+                }
+                // Closing the connection  
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
