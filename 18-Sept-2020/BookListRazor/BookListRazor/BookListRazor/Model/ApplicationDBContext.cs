b4f7c67bb6a2fe1af431370f52eedcd30182f52e
Utkarsh_Pant <utkarshpant1994@gmail.com>
2020-09-19 10:24:56 +0530

    ASP.Net core Razor files added

---
+++ 18-Sept-2020/BookListRazor/BookListRazor/BookListRazor/Model/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDBContext: DbContext
+    {
+        //.....parameter for Constructor is needed for Dependency Injection
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+                
+        }
+
+        //.....To add any model to database inside DbContext  , we need an Entry
+        public DbSet<Book> Book { get; set; }  //.......Entry.........
+
+        //.......Now add DbContext inside startup.cs
+    }
+}
